50d44c3e237e4a0a70b4b7ce1c38d969b484dc4b
Abhay D <a.deshpande012@gmail.com>
2025-04-27 23:32:50 -0700

    Update run script

--- run_evals_levels_abhay.sh
+++ run_evals_levels_abhay.sh
@@ -1,9 +1,15 @@
-python gcngrasp/run_tg_eval_ver_ar.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_t_split_idx_0_.yml --use_levels
-python gcngrasp/run_tg_eval_ver_ar.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_t_split_idx_1_.yml --use_levels
-python gcngrasp/run_tg_eval_ver_ar.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_t_split_idx_2_.yml --use_levels
-python gcngrasp/run_tg_eval_ver_ar.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_t_split_idx_3_.yml --use_levels
-python gcngrasp/run_tg_eval_ver_ar.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_o_split_idx_0_.yml --use_levels
-python gcngrasp/run_tg_eval_ver_ar.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_o_split_idx_1_.yml --use_levels
-python gcngrasp/run_tg_eval_ver_ar.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_o_split_idx_2_.yml --use_levels
-python gcngrasp/run_tg_eval_ver_ar.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_o_split_idx_3_.yml --use_levels
+set -euxo pipefail
+
+cp -r /weka/prior/abhayd/semantic-grasping-datasets/graspgpt_checkpoints checkpoints
+mkdir -p data
+ln -s /weka/prior/abhayd/semantic-grasping-datasets/LA-TaskGrasp data/taskgrasp
+
+python gcngrasp/run_tg_eval.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_t_split_idx_0_.yml --use_levels
+python gcngrasp/run_tg_eval.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_t_split_idx_1_.yml --use_levels
+python gcngrasp/run_tg_eval.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_t_split_idx_2_.yml --use_levels
+python gcngrasp/run_tg_eval.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_t_split_idx_3_.yml --use_levels
+python gcngrasp/run_tg_eval.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_o_split_idx_0_.yml --use_levels
+python gcngrasp/run_tg_eval.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_o_split_idx_1_.yml --use_levels
+python gcngrasp/run_tg_eval.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_o_split_idx_2_.yml --use_levels
+python gcngrasp/run_tg_eval.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_o_split_idx_3_.yml --use_levels
 python gcngrasp/results/aggregate_results_levels.py
