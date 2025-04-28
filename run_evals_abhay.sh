906b9cc0f0f06884431e1d09d32dac3baa9799ed
Abhay D <a.deshpande012@gmail.com>
2025-04-27 18:03:15 -0700

    symlink la-taskgrasp

--- run_evals_abhay.sh
+++ run_evals_abhay.sh
@@ -1,6 +1,9 @@
 set -euxo pipefail
 
 cp -r /weka/prior/abhayd/semantic-grasping-datasets/graspgpt_checkpoints checkpoints
+mkdir -p data
+ln -s /weka/prior/abhayd/semantic-grasping-datasets/LA-TaskGrasp data/taskgrasp
+
 python gcngrasp/run_tg_eval.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_t_split_idx_0_.yml
 python gcngrasp/run_tg_eval.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_t_split_idx_1_.yml
 python gcngrasp/run_tg_eval.py --cfg_file cfg/eval/gcngrasp/gcngrasp_split_mode_t_split_idx_2_.yml
